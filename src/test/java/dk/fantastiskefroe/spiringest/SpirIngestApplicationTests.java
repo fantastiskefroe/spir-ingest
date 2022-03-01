548868cce46a10232fec9f7492d170dc015d5028
Henrik Sørensen <hhs19942@gmail.com>
2022-03-01 20:52:30 +0100

    spring initializr

---
+++ src/test/java/dk/fantastiskefroe/spiringest/SpirIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.fantastiskefroe.spiringest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
