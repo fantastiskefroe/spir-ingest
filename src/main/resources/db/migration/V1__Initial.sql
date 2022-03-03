b4da4581cbb27b425b7415ede8878481a3303d47
Henrik Sørensen <hhs19942@gmail.com>
2022-03-03 12:47:04 +0100

    Reset migrations

--- src/main/resources/db/migration/V1__Initial.sql
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -1,15 +1,48 @@
-CREATE TABLE orders (
-    id BIGINT PRIMARY KEY,
-    order_number INT
+CREATE TABLE "order"
+(
+    id                   BIGSERIAL   NOT NULL,
+    name                 VARCHAR(16) NOT NULL,
+    number               INT         NOT NULL,
+    status               VARCHAR(50) NOT NULL,
+    cancel_reason        VARCHAR(10) NULL,
+    financial_status     VARCHAR(20) NOT NULL,
+    total_discount       INT         NULL,
+    subtotal_price       INT         NULL,
+    total_tax            INT         NULL,
+    total_price          INT         NULL,
+    total_shipping_price INT         NULL,
+    created_date_time    TIMESTAMPTZ NOT NULL,
+    valid_from           TIMESTAMPTZ NOT NULL,
+    valid_to             TIMESTAMPTZ NULL,
+
+    CONSTRAINT order_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT order_name_key
+        UNIQUE (name, valid_from, valid_to)
 );
 
-CREATE TABLE line_items (
-    id BIGINT PRIMARY KEY,
-    order_id BIGINT,
-    product_id BIGINT,
-    variant_id BIGINT,
-    sku VARCHAR(50),
+CREATE UNIQUE INDEX order_id_uindex
+    ON "order" (id);
 
-    FOREIGN KEY (order_id)
-        REFERENCES orders (id)
+CREATE TABLE order_line
+(
+    id            BIGSERIAL    NOT NULL,
+    order_id      BIGINT       NOT NULL,
+    sku           VARCHAR(64)  NOT NULL,
+    title         VARCHAR(255) NOT NULL,
+    variant_title VARCHAR(255) NOT NULL,
+    quantity      INT          NOT NULL,
+    price         INT          NULL,
+
+    CONSTRAINT order_line_pk
+        PRIMARY KEY (id),
+
+    CONSTRAINT order_line_order_id_fk
+        FOREIGN KEY (order_id) REFERENCES "order" (id)
+            ON DELETE CASCADE
 );
+
+CREATE UNIQUE INDEX order_line_id_uindex
+    ON order_line (id);
+
