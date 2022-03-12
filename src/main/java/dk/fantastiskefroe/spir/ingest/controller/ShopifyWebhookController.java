1d46a429c000eaf0b7eeed73a39cdcaca6109f4e
Anton Christensen <anton.christensen9700@gmail.com>
2022-03-12 21:48:26 +0100

    Makes hmac validation only apply to webhook prefixed paths

--- src/main/java/dk/fantastiskefroe/spir/ingest/controller/ShopifyWebhookController.java
+++ src/main/java/dk/fantastiskefroe/spir/ingest/controller/ShopifyWebhookController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @Validated
-@RequestMapping
+@RequestMapping("/webhook")
 public class ShopifyWebhookController {
     private final OrderService orderService;
 
