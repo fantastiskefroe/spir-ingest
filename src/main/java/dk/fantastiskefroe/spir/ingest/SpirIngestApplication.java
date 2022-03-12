2d6aa5d3ad5c56c6a3d31212c79356aff9e83a0c
Henrik Sørensen <hhs19942@gmail.com>
2022-03-12 21:03:14 +0100

    Completely fix everything

--- src/main/java/dk/fantastiskefroe/spir/ingest/SpirIngestApplication.java
+++ src/main/java/dk/fantastiskefroe/spir/ingest/SpirIngestApplication.java
@@ -1,9 +1,12 @@
 package dk.fantastiskefroe.spir.ingest;
 
+import dk.fantastiskefroe.spir.ingest.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ApplicationProperties.class)
 public class SpirIngestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
+++ src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
@@ -1,23 +1,80 @@
 package dk.fantastiskefroe.spir.ingest.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    private final String hmacKey;
+
+    public SecurityConfig(ApplicationProperties applicationProperties) {
+        this.hmacKey = applicationProperties.getHmacPrivateKey();
+    }
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
+        return http
                 .cors().disable()
                 .csrf().disable()
-                .authorizeExchange(exchanges -> exchanges
-                        .matchers(ServerWebExchangeMatchers.anyExchange()).permitAll()
-                );
+                .addFilterAt((exchange, chain) -> {
+                    final CachingServerWebExchangeDecorator cachingServerWebExchangeDecorator = new CachingServerWebExchangeDecorator(exchange);
+                    final String hmac = cachingServerWebExchangeDecorator.getRequest().getHeaders().getFirst("X-Shopify-Hmac-Sha256");
 
-        return http.build();
+                    return cachingServerWebExchangeDecorator.getRequest()
+                            .getBody()
+                            .map(SecurityConfig::databufferToString)
+                            .collectList()
+                            .map(l -> String.join("", l))
+                            .map(body -> {
+                                if (validateWebhook(body, hmac, hmacKey)) {
+                                    return cachingServerWebExchangeDecorator;
+                                }
+                                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+                            })
+                            .flatMap(chain::filter);
+                }, SecurityWebFiltersOrder.LAST)
+                .authorizeExchange().anyExchange().permitAll()
+                .and().build();
+    }
+
+    private static String calculateHmac(String data, String key) {
+        final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "HmacSHA256");
+        final Mac mac;
+        try {
+            mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Base64.getEncoder().encodeToString(mac.doFinal(data.getBytes()));
+    }
+
+    private static boolean validateWebhook(String body, String hmac, String key) {
+        final String calculatedHmac = calculateHmac(body, key);
+
+        return calculatedHmac.equals(hmac);
+    }
+
+    public static String databufferToString(org.springframework.core.io.buffer.DataBuffer dataBuffer) {
+        final byte[] bytes = new byte[dataBuffer.readableByteCount()];
+        dataBuffer.read(bytes);
+        DataBufferUtils.release(dataBuffer);
+
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 }
