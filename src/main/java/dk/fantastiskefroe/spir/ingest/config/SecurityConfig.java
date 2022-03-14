e6f20eae94284f248bff44025aaa2ec94f652811
Henrik Sørensen <hhs19942@gmail.com>
2022-03-14 11:27:35 +0100

    Move initialization to constructor

--- src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
+++ src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package dk.fantastiskefroe.spir.ingest.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerWebExchange;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -22,10 +19,17 @@ import java.util.Base64;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private final String hmacKey;
+    private static final Base64.Encoder ENCODER = Base64.getEncoder();
+    private final Mac mac;
 
     public SecurityConfig(ApplicationProperties applicationProperties) {
-        this.hmacKey = applicationProperties.getHmacPrivateKey();
+        final SecretKeySpec secretKeySpec = new SecretKeySpec(applicationProperties.getHmacPrivateKey().getBytes(), "HmacSHA256");
+        try {
+            mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Bean
@@ -33,48 +37,37 @@ public class SecurityConfig {
         return http
                 .cors().disable()
                 .csrf().disable()
-                .addFilterAt((exchange, chain) ->
-                        chain.filter(new CachingServerWebExchangeDecorator(exchange))
-                        , SecurityWebFiltersOrder.FIRST)
+                .addFilterAt(
+                        (exchange, chain) -> chain.filter(new CachingServerWebExchangeDecorator(exchange)),
+                        SecurityWebFiltersOrder.FIRST)
                 .authorizeExchange()
                 .pathMatchers("/webhook/**")
                 .access((authentication, context) -> {
                     final String hmac = context.getExchange().getRequest().getHeaders().getFirst("X-Shopify-Hmac-Sha256");
                     return context.getExchange().getRequest()
                             .getBody()
-                            .map(SecurityConfig::databufferToString)
+                            .map(SecurityConfig::dataBufferToString)
                             .collectList()
                             .map(l -> String.join("", l))
-                            .map(body -> new AuthorizationDecision(validateWebhook(body, hmac, hmacKey)));
+                            .map(body -> new AuthorizationDecision(validateWebhook(body, hmac)));
                 })
                 .anyExchange().permitAll()
                 .and().build();
     }
 
-    private static String calculateHmac(String data, String key) {
-        final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "HmacSHA256");
-        final Mac mac;
-        try {
-            mac = Mac.getInstance("HmacSHA256");
-            mac.init(secretKeySpec);
-        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            throw new RuntimeException(e);
-        }
-
-        return Base64.getEncoder().encodeToString(mac.doFinal(data.getBytes()));
-    }
-
-    private static boolean validateWebhook(String body, String hmac, String key) {
-        final String calculatedHmac = calculateHmac(body, key);
-
-        return calculatedHmac.equals(hmac);
-    }
-
-    public static String databufferToString(org.springframework.core.io.buffer.DataBuffer dataBuffer) {
+    private static String dataBufferToString(DataBuffer dataBuffer) {
         final byte[] bytes = new byte[dataBuffer.readableByteCount()];
         dataBuffer.read(bytes);
         DataBufferUtils.release(dataBuffer);
 
         return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    private boolean validateWebhook(String data, String hmac) {
+        return calculateHmac(data).equals(hmac);
+    }
+
+    private String calculateHmac(String data) {
+        return ENCODER.encodeToString(mac.doFinal(data.getBytes()));
     }
 }
