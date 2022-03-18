520fe42934f36f528c70ff57032842f9e50b5a0b
Anton Christensen <anton.christensen9700@gmail.com>
2022-03-18 09:32:40 +0100

    Allow cors from localhost and spir-app

--- src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
+++ src/main/java/dk/fantastiskefroe/spir/ingest/config/SecurityConfig.java
@@ -2,21 +2,21 @@ package dk.fantastiskefroe.spir.ingest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.buffer.DataBufferUtils;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Base64;
 
 @EnableWebFluxSecurity
@@ -26,6 +26,21 @@ public class SecurityConfig {
 
     public SecurityConfig(ApplicationProperties applicationProperties) {
         this.hmacKey = applicationProperties.getHmacPrivateKey();
+    }
+
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:8080", "https://spir.fantastiskefroe.dk"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 
     @Bean
